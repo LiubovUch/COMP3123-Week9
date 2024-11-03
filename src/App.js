e29736cad044d5179ca022bfc16c4d5b42d7bd67
Liubov Uchamprina <liuchamprina@gmail.com>
2024-11-02 21:54:34 -0400

    lab completed

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const studentInfo = {
+    studentId: '101445260',
+    name: 'Liubov Uchamprina',
+    college: 'George Brown College, Toronto'
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <p>{studentInfo.studentId}</p>
+        <p>{studentInfo.name}</p>
+        <p>{studentInfo.college}</p>
       </header>
     </div>
   );
